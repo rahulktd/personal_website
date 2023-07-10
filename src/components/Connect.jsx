1629844c3cedd482d01e5fa02c06be1d2b3115ef
rahulktd <rahulkpktd@gmail.com>
2023-07-10 09:29:23 +0530

    connect sections

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
 
 function Connect() {
   return (
-    <div>Connect</div>
+    <div className="mx-auto max-w-7xl">
+
+<section>
+      <h1 className="text-4xl font-bold mt-5">Let's Talk</h1>
+      <h1 className="font-bold bg-text">Contact</h1>
+      <div className="text-left">
+        <div className="pt-5">
+          <h2 className="mb-5">Want to connect?</h2>
+          <ul className="flex items-center space-x-4">
+            <li>
+              <a target="_blank" rel="noopener noreferrer" href="mailto:rahulkpktd@gmail.com">
+              <HiOutlineMail className="w-6 h-6" />
+              </a>
+            </li>
+            <li>
+              <a target="_blank" rel="noopener noreferrer" href="https://github.com/rahulktd">
+              <FaGithub className="w-6 h-6" />
+              </a>
+            </li>
+            <li>
+              <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/rahul-k-p-9aaa32236/">
+              <FaLinkedin className="w-6 h-6" /> 
+              </a>
+            </li>
+            
+            <li>
+              <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/rahul_1_8">
+              <FaTwitter className="w-6 h-6" /> 
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <br />
+    </section>
+
+    </div>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="bg-white p-4">
             <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-xl font-bold">Resume</h1>
+        <h1 className="text-xl font-bold"><a rel="noopener noreferrer"  target="_blank" href="https://drive.google.com/file/d/1dUA-D4twt8VLJz3U9wVBaQ2n2Yd9W9XD/view?usp=sharing">Resume</a></h1>
         <nav>
           {/* <ul className="flex space-x-4">
             <li>
