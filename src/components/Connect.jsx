8049d4bb00f1f74af253ed60f2cd10a904ce6677
rahulktd <rahulkpktd@gmail.com>
2023-07-10 13:13:20 +0530

    some changes in template

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -14,23 +14,23 @@ function Connect() {
           <h2 className="mb-5">Want to connect?</h2>
           <ul className="flex items-center space-x-4">
             <li>
-              <a target="_blank" rel="noopener noreferrer" href="mailto:rahulkpktd@gmail.com">
+              <a target="_blank" className="hover:text-blue-500" rel="noopener noreferrer" href="mailto:rahulkpktd@gmail.com">
               <HiOutlineMail className="w-6 h-6" />
               </a>
             </li>
             <li>
-              <a target="_blank" rel="noopener noreferrer" href="https://github.com/rahulktd">
+              <a target="_blank" rel="noopener noreferrer" className="hover:text-blue-500" href="https://github.com/rahulktd">
               <FaGithub className="w-6 h-6" />
               </a>
             </li>
             <li>
-              <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/rahul-k-p-9aaa32236/">
+              <a target="_blank" rel="noopener noreferrer" className="hover:text-blue-500" href="https://www.linkedin.com/in/rahul-k-p-9aaa32236/">
               <FaLinkedin className="w-6 h-6" /> 
               </a>
             </li>
             
             <li>
-              <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/rahul_1_8">
+              <a target="_blank" rel="noopener noreferrer" className="hover:text-blue-500" href="https://twitter.com/rahul_1_8">
               <FaTwitter className="w-6 h-6" /> 
               </a>
             </li>
@@ -39,8 +39,8 @@ function Connect() {
       </div>
       <br />
     </section>
-
     </div>
+    
   )
 }
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,15 +12,14 @@ function Projects() {
           <div className="mt-3">
             <div className="py-4">
               <div className="desc">
-                <h4 class="font-semibold">Workshop Management using Django</h4>
-                <br />
-                <p>
-                  <a target="_blank" rel="noopener noreferrer" class="inline-block px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg" href="https://github.com/rahulktd/Workshop-management-">
+                <h4 className="font-semibold">Workshop Management using Django</h4>
+                <p className='py-3'>
+                  <a target="_blank" rel="noopener noreferrer" className="inline-block px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg" href="https://github.com/rahulktd/Workshop-management-">
                     Github Link
                   </a>
                 </p>
               </div>
-              <ul>
+              <ul className="list-disc">
                 <li>This project is a comprehensive workshop management system built using Django, a high-level Python web framework.</li>
                 <li>It provides features to manage workshops, including participant registration, workshop scheduling, and resource allocation.</li>
                 <li> With an intuitive user interface and robust functionality, this application simplifies the process of organizing and overseeing workshops effectively.</li>
@@ -29,15 +28,14 @@ function Projects() {
 
             <div className="py-4">
               <div className="desc">
-                <h4 class="font-semibold">Job Board</h4>
-                <br />
-                <p>
-                  <a target="_blank" class="inline-block px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg" rel="noopener noreferrer" href="https://github.com/rahulktd/Job-Board">
+                <h4 className="font-semibold">Job Board</h4>
+                <p className='py-3'>
+                  <a target="_blank" className="inline-block px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg" rel="noopener noreferrer" href="https://github.com/rahulktd/Job-Board">
                     Github Link
                   </a>
                 </p>
               </div>
-              <ul>
+              <ul className="list-disc">
                 <li>The Job Board project is a powerful Django-based web application designed to connect job seekers with employment opportunities. </li>
                 <li> Employers can post job listings, while job seekers can search for relevant positions and submit their applications. </li>
                  <li> With advanced filtering options, seamless user experience, and secure authentication, this platform streamlines the job search process for both employers and job seekers.</li>                
@@ -46,11 +44,11 @@ function Projects() {
 
             <div className="py-4">
               <div className="desc">
-                <h4 class="font-semibold">Social Media using Django</h4>
-                <br />
-                <p>
-                  <a
-                  class="inline-block px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg"
+                <h4 className="font-semibold">Social Media using Django</h4>
+                
+                <p className='py-3'>
+                  <a 
+                  className="inline-block px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg"
                     target="_blank"
                     rel="noopener noreferrer"
                     href="https://github.com/rahulktd/social_media_django_project"
@@ -59,7 +57,7 @@ function Projects() {
                   </a>
                 </p>
               </div>
-              <ul>
+              <ul className="list-disc">
               <li>Social Media is a dynamic Django project that enables users to connect and engage in a social networking environment.</li>
               <li>This application offers features such as user profiles, friend connections, post sharing, and real-time notifications.</li>
               <li> By leveraging Django's capabilities, this project provides a scalable and interactive social media experience.</li>
@@ -67,13 +65,13 @@ function Projects() {
             </div>
 
             <div className="py-4">
-              <div class="font-semibold" className="desc">
-                <h4>E-Commerce App using Django</h4>
-                <br />
-                <p>
+              <div className="font-semibold">
+                <h4 >E-Commerce App using Django</h4>
+                
+                <p className='py-3'>
                   <a
                     target="_blank"
-                    class="inline-block px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg"
+                    className="inline-block px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg"
                     rel="noopener noreferrer"
                     href="https://github.com/rahulktd/E-commerce-website-django"
                   >
@@ -81,7 +79,7 @@ function Projects() {
                   </a>
                 </p>
               </div>
-              <ul>
+              <ul className="list-disc">
               <li>The E-Commerce App project is a feature-rich online shopping platform built with Django.</li>
               <li>It includes essential functionalities like product catalog management, shopping cart functionality, secure payment processing, and order tracking. </li>
               <li> With an intuitive interface and seamless integration, this application provides a delightful online shopping experience for customers and a robust backend for business owners.</li>
@@ -90,12 +88,12 @@ function Projects() {
 
             <div className="py-4">
               <div className="desc">
-                <h4 class="font-semibold">Real-time Chat App using Django</h4>
-                <br />
-                <p>
+                <h4 className="font-semibold">Real-time Chat App using Django</h4>
+          
+                <p className='py-3'>
                   <a
                     target="_blank"
-                    class="inline-block px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg"
+                    className="inline-block px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg"
                     rel="noopener noreferrer"
                     href="https://github.com/rahulktd/Real-time_Chat_App_django"
                   >
@@ -103,7 +101,7 @@ function Projects() {
                   </a>
                 </p>
               </div>
-              <ul>
+              <ul className="list-disc">
               <li>Real-time Chat App using Django enables users to engage in instant messaging and group conversations.</li>
               <li>Leveraging Django Channels, it provides features like private messaging, online presence indicators, and message history. </li>
               <li>With its responsive design and real-time updates, this chat app fosters seamless communication among users.</li>
@@ -112,12 +110,12 @@ function Projects() {
 
             <div className="py-4">
               <div className="desc">
-                <h4 class="font-semibold">E-Commerce website using React</h4>
-                <br />
-                <p>
+                <h4 className="font-semibold">E-Commerce website using React</h4>
+             
+                <p className='py-3'>
                   <a
                     target="_blank"
-                    class="inline-block px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg"
+                    className="inline-block px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg"
                     rel="noopener noreferrer"
                     href="https://github.com/rahulktd/ecom-app-react"
                   >
@@ -125,7 +123,7 @@ function Projects() {
                   </a>
                 </p>
               </div>
-              <ul>
+              <ul className="list-disc">
                 <li>E-Commerce website using React is a full-fledged online shopping platform developed with React.</li>
                 <li>It offers a modern and intuitive user interface, along with features like product browsing, filtering, and secure checkout.</li>
                 <li>With its responsive design and optimized performance, this website provides an engaging and efficient online shopping experience.</li>
@@ -134,20 +132,20 @@ function Projects() {
 
             <div className="py-4">
               <div className="desc">
-                <h4 class="font-semibold">Restaurant website using Material UI-React Project</h4>
-                <br />
-                <p>
+                <h4 className="font-semibold">Restaurant website using Material UI-React Project</h4>
+             
+                <p className='py-3'>
                   <a
                     target="_blank"
                     rel="noopener noreferrer"
-                    class="inline-block px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg"
+                    className="inline-block px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg"
                     href="https://github.com/rahulktd/restaurant-materialUI-react"
                   >
                     Github Link
                   </a>
                 </p>
               </div>
-              <ul>
+              <ul className="list-disc">
                 <li>The Restaurant website using Material UI-React Project showcases a visually appealing and user-friendly restaurant website.</li>
                 <li>It offers a seamless browsing experience, including features like menu presentation, table reservations, and contact information.</li>
                 <li>With its elegant design and responsive layout, this website enhances the online presence of a restaurant and attracts potential customers.</li>
@@ -156,20 +154,20 @@ function Projects() {
 
             <div className="py-4">
               <div className="desc">
-                <h4 class="font-semibold">Todo App using Django</h4>
-                <br />
-                <p>
+                <h4 className="font-semibold">Todo App using Django</h4>
+           
+                <p className='py-3'>
                   <a
                     target="_blank"
                     rel="noopener noreferrer"
-                    class="inline-block px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg"
+                    className="inline-block px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg"
                     href="https://github.com/rahulktd/ToDo-App"
                   >
                     Github Link
                   </a>
                 </p>
               </div>
-              <ul>
+              <ul className="list-disc">
                 <li>Todo App using Django is a simple yet powerful task management application.</li>
                 <li>It allows users to create, organize, and track their to-do lists effectively.</li>
                 <li>With features such as task prioritization, due date reminders, and user authentication, this app helps users stay organized and focused.</li>
@@ -178,12 +176,12 @@ function Projects() {
 
             <div className="py-4">
               <div className="desc">
-                <h4 class="font-semibold">Todo App using Flask</h4>
-                <br />
-                <p>
+                <h4 className="font-semibold">Todo App using Flask</h4>
+   
+                <p className='py-3'>
                   <a
                     target="_blank"
-                    class="inline-block px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg"
+                    className="inline-block px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg"
                     rel="noopener noreferrer"
                     href="https://github.com/rahulktd/Todo-app-flask"
                   >
@@ -191,7 +189,7 @@ function Projects() {
                   </a>
                 </p>
               </div>
-              <ul>
+              <ul className="list-disc">
                 <li>Todo App using Flask is a lightweight task management application developed with Flask, a micro web framework in Python.</li>
                 <li>It provides essential functionality like task creation, editing, and marking tasks as completed.</li>
                 <li>With its minimalistic design and intuitive user interface, this app helps users manage their tasks efficiently.</li>
